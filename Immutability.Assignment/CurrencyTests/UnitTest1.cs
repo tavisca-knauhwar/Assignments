9273686e7868c191bf1de2779820d53565cd1396
Kuldeep Nauhwar <knauhwar@tavisca.com>
2017-08-15 21:56:35 +0530

    Assignemnt 4

---
+++ Immutability.Assignment/CurrencyTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Immutability.Assignment;
+
+namespace CurrencyTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Currency _currency;
+        [TestInitialize]
+        public void Initialize()
+        {
+            _currency = new Currency();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Immutability.Assignment/Immutability.Assignment/Currency.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Immutability.Assignment
+{
+    public class Currency
+    {
+        private readonly Dictionary<string, string> _countriesCurrency;
+        public  Dictionary<string, string> CountriesCurrency { get { return _countriesCurrency; } }
+
+        public Currency()
+        {
+            _countriesCurrency = new Dictionary<string, string>();
+            var doc = XDocument.Load(@"D:\GIT\Assignments\Immutability.Assignment\Immutability.Assignment\CountiresCurrency.xml");
+            var countries = doc.Root.Elements().Select(x => x.Element("CtryNm"));
+        }
+
+    }
+}
